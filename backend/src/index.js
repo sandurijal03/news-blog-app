88cab33a3fab9d31c577f7d548e79bcd78406752
Sandip Rijal <sandurijal03@hotmail.com>
2021-07-28 05:17:25 +0545

    added main app inside function

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,33 @@ const { config } = require('dotenv');
 config();
 
 const express = require('express');
+const cors = require('cors');
+const paginate = require('express-paginate');
+const passport = require('passport');
+const connectDB = require('./db/connectDB');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success: true });
-});
+const runServer = async () => {
+  try {
+    connectDB();
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(passport.initialize());
 
-const port = process.env.PORT;
-app.listen(port, () => console.log(`listening to server on port ${port}`));
+    app.use(paginate.middleware(process.env.LIMIT, process.env.MAX_LIMIT));
+
+    app.get('/', (req, res) => {
+      res.status(200).json({ success: true });
+    });
+
+    const port = process.env.PORT;
+    app.listen(port, () => console.log(`listening to server on port ${port}`));
+  } catch (err) {
+    console.log(err.message);
+    runServer();
+  }
+};
+
+runServer();
