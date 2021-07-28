d26a2e0df62ef553338d26a48dc159b79b94ac3e
Sandip Rijal <sandurijal03@hotmail.com>
2021-07-29 04:47:54 +0545

    binded all routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,14 @@ const paginate = require('express-paginate');
 const passport = require('passport');
 const connectDB = require('./db/connectDB');
 
+const authRouter = require('./routes/auth/auth.routes');
+const adminRouter = require('./routes/admin/admin.routes');
+const categoryRouter = require('./routes/category/category.routes');
+const commentRouter = require('./routes/comments/comments.routes');
+const profileRouter = require('./routes/profile/profile.routes');
+const storyRouter = require('./routes/story/story.routes');
+const videoRouter = require('./routes/video/video.routes');
+
 const app = express();
 
 const runServer = async () => {
@@ -19,9 +27,13 @@ const runServer = async () => {
 
     app.use(paginate.middleware(process.env.LIMIT, process.env.MAX_LIMIT));
 
-    app.get('/', (req, res) => {
-      res.status(200).json({ success: true });
-    });
+    app.use('/api', authRouter);
+    app.use('/api', adminRouter);
+    app.use('/api/category', categoryRouter);
+    app.use('/api/comments', commentRouter);
+    app.use('/api/profile', profileRouter);
+    app.use('/api/stories', storyRouter);
+    app.use('/api/videos', videoRouter);
 
     const port = process.env.PORT;
     app.listen(port, () => console.log(`listening to server on port ${port}`));
