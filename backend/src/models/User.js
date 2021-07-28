b7e49e7e741b03cba9b78b2ae5ae52668ca0929b
Sandip Rijal <sandurijal03@hotmail.com>
2021-07-29 04:42:03 +0545

    added default user image

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -14,6 +14,8 @@ const userSchema = new Schema(
     },
     image: {
       type: String,
+      default:
+        'https://www.kindpng.com/picc/m/105-1055656_account-user-profile-avatar-avatar-user-profile-icon.png',
     },
     role: {
       type: String,
