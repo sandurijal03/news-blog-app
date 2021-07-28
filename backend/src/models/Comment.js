a26f93129cd357768d6f9f0698b6b8a00757a4c6
Sandip Rijal <sandurijal03@hotmail.com>
2021-07-28 05:45:28 +0545

    added Comment Schema

---
+++ backend/src/models/Comment.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const commentSchema = new Schema(
+  {
+    body: {
+      type: String,
+      required: true,
+    },
+    story: {
+      type: ObjectId,
+      ref: 'Story',
+    },
+    createdBy: {
+      type: ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = model('Comment', commentSchema);
